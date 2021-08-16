b4770db2fcdefe2abc55db1f82d7d3505a377ed2
KyleKilmartin371 <kylekilmartin@yahoo.com>
2021-08-15 17:45:02 -0700

    added questions

--- index.js
+++ index.js
@@ -18,8 +18,44 @@ const questions = () =>{
                     return false;
                 }
             }
-        }
-    ])
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Tell us a little about your project',
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'What are the steps required to install your project? (Required)',
+            validate: instalInput =>{
+                if (instalInput){
+                    return true;
+                }else{
+                    console.log('Please enter your project steps!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Please provide instructions and examples for use. Include screenshots as needed (Required)',
+            validate: useInput =>{
+                if (useInput){
+                    return true;
+                }else{
+                    console.log('Please enter your projects details!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'credits',
+            message: 'List the collaborators, if any, with links to their Github profiles.',
+            }
+    ]);
 };
 
 // TODO: Create a function to write README file
@@ -29,4 +65,4 @@ function writeToFile(fileName, data) {}
 function init() {}
 
 // Function call to initialize app
-questions();
+questions()
